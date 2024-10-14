34838fa6dbcad5e0be04cdb343edf9127760e2ad
hellcat29A <bhiri22@gmail.com>
2024-10-14 15:12:14 +0100

    commity

--- packages/ze-package/src/index.ts
+++ packages/ze-package/src/index.ts
@@ -2,4 +2,5 @@ export const zeFunction = () => {
   console.log("very important logic 🚀");
   console.log("And yet another important logic 🚀");
   console.log("testing lerna publish to npm");
+  console.log("Made some changes to the package");
 };
